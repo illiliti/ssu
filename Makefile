dabfa6380751e78244d5bc4e5da9139eb46bdb25
illiliti <illiliti@protonmail.com>
2020-08-28 02:03:56 +0300

    init

---
+++ Makefile
@@ -0,0 +1,23 @@
+.POSIX:
+
+PREFIX = /usr/local
+BINDIR = ${PREFIX}/bin
+XCFLAGS = ${CPPFLAGS} ${CFLAGS} -std=c99 -D_POSIX_C_SOURCE=200809L \
+		  -Wall -Wextra -Wpedantic -Wmissing-prototypes -Wstrict-prototypes
+
+all: sls
+
+sls: sls.c
+	${CC} ${XCFLAGS} -o $@ $< ${LDFLAGS}
+
+install: sls
+	mkdir -p  ${DESTDIR}${BINDIR}
+	cp -f sls ${DESTDIR}${BINDIR}
+
+uninstall:
+	rm -f ${DESTDIR}${BINDIR}/sls
+
+clean:
+	rm -f sls
+
+.PHONY: all clean install uninstall
---
+++ sls.c
@@ -0,0 +1,157 @@
+#include <pwd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#ifndef ENV_PATH
+#define ENV_PATH "/bin:/sbin:/usr/bin:/usr/sbin"
+#endif
+
+static int exec_file(char **argv)
+{
+    pid_t pid;
+    int ret;
+
+    pid = fork();
+
+    if (pid == -1) {
+        perror("fork");
+        return 1;
+    }
+
+    if (pid == 0) {
+        execvp(argv[0], argv);
+        perror("execvp");
+        return errno == ENOENT ? 127 : 126;
+    }
+    else {
+        waitpid(pid, &ret, 0);
+        return WEXITSTATUS(ret);
+    }
+}
+
+static int edit_file(char **argv)
+{
+    char *editor;
+
+    // TODO copy file to /tmp and edit as regular user
+
+    editor = getenv("EDITOR");
+    argv[0] = editor ? editor : "vi";
+
+    return exec_file(argv);
+}
+
+static int run_shell(char *shell)
+{
+    char *argv[2];
+
+    argv[0] = shell;
+    argv[1] = NULL;
+
+    return exec_file(argv);
+}
+
+static void print_usage(const char *name)
+{
+    fprintf(stderr, "Usage: %s [-p] [-u user] -s\n", name);
+    fprintf(stderr, "Usage: %s [-p] [-u user] -e file [file ...]\n", name);
+    fprintf(stderr, "Usage: %s [-p] [-u user] -c command [args ...]\n", name);
+}
+
+int main(int argc, char **argv)
+{
+    int pflag = 0, eflag = 0, cflag = 0, sflag = 0;
+    char *shell, *term, *user = NULL;
+    extern char **environ;
+    struct passwd *pw;
+    int opt;
+
+    if (argc < 2) {
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    while ((opt = getopt(argc, argv, ":u:pecs")) != -1) {
+        switch (opt) {
+        case 'p':
+            pflag = 1;
+            break;
+        case 'e':
+            eflag = 1;
+            break;
+        case 'c':
+            cflag = 1;
+            break;
+        case 's':
+            sflag = 1;
+            break;
+        case 'u':
+            user = optarg;
+            break;
+        case '?':
+            fprintf(stderr, "%s: Unrecognized option: %c\n", argv[0], optopt);
+            print_usage(argv[0]);
+            return 1;
+        case ':':
+            fprintf(stderr, "%s: Option requires an argument: %c\n", argv[0], optopt);
+            print_usage(argv[0]);
+            return 1;
+        }
+    }
+
+    if (getuid() != 0 && getgid() != getegid()) {
+        fprintf(stderr, "%s: Permission denied\n", argv[0]);
+        return 1;
+    }
+
+    pw = getpwnam(user ? user : "root");
+
+    if (!pw) {
+        perror("getpwnum");
+        return 1;
+    }
+
+    if (setuid(pw->pw_uid) == -1) {
+        perror("setuid");
+        return 1;
+    }
+
+    if (setgid(pw->pw_gid) == -1) {
+        perror("setgid");
+        return 1;
+    }
+
+    if (!pflag) {
+        shell = pw->pw_shell ? pw->pw_shell : "/bin/sh";
+        term = getenv("TERM");
+        environ = NULL;
+
+        if (term) {
+            setenv("TERM", term, 0);
+        }
+
+        setenv("LOGNAME", pw->pw_name, 0);
+        setenv("USER", pw->pw_name, 0);
+        setenv("HOME", pw->pw_dir, 0);
+        setenv("PATH", ENV_PATH, 0);
+        setenv("SHELL", shell, 0);
+    }
+
+    if (sflag) {
+        return run_shell(pflag ? getenv("SHELL") : shell);
+    }
+
+    if (eflag) {
+        return edit_file(argv + optind - 1);
+    }
+
+    if (cflag) {
+        return exec_file(argv + optind);
+    }
+
+    print_usage(argv[0]);
+    return 1;
+}
